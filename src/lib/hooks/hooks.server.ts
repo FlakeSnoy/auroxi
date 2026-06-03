a61f71e76899d62139e10f8bf37b27f753a154a0
FlameFragsC <Malebochester@proton.me>
2026-06-03 20:17:04 +0200

    feat: Intergrate files and remove unnecessary files

--- src/lib/hooks/hooks.server.ts
+++
@@ -1,8 +0,0 @@
-import { auth } from '$lib/server/auth.js';
-import { svelteKitHandler } from 'better-auth/svelte-kit';
-import type { Handle } from '@sveltejs/kit';
-import { building } from '$app/environment';
-
-export const handle: Handle = async ({ event, resolve }) => {
-  return svelteKitHandler({ event, resolve, auth, building });
-};
