f0eda5122611197804277e14886f7941125879c9
FlameFragsC <Malebochester@proton.me>
2026-06-06 14:13:00 +0200

    update file

--- src/lib/components/layout/Frame.svelte
+++ src/lib/components/layout/Frame.svelte
@@ -2,6 +2,8 @@
   const { children } = $props();
 </script>
 
-<div class="flex-1 flex flex-col md:ml-56 min-h-screen bg-zinc-900">
-  {@render children()}
+<div class="flex-1 flex flex-col md:ml-56">
+  <div class="flex-1 flex flex-col border-l border-t border-zinc-800 rounded-tl-2xl overflow-hidden">
+    {@render children()}
+  </div>
 </div>
