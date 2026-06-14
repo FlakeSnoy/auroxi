02082b4fc61ab463f3b7c89d9c6b5bdfef29cfdd
FlameFragsC <Malebochester@proton.me>
2026-06-14 17:17:38 +0200

    feat: patches

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,14 @@
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 import * as schema from './schema.js';
-import { env } from '$env/dynamic/private';
+import { DATABASE_URL, TURSO_AUTH_TOKEN } from '$env/static/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!TURSO_AUTH_TOKEN) throw new Error('TURSO_AUTH_TOKEN is not set');
 
-const client = createClient({ url: env.DATABASE_URL });
+const client = createClient({
+	url: DATABASE_URL,
+	authToken: TURSO_AUTH_TOKEN,
+});
 
 export const db = drizzle(client, { schema });
