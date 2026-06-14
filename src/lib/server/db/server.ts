9a193ab2bd3b075a21045f0723520a047a516ae9
FlameFragsC <Malebochester@proton.me>
2026-06-14 16:58:06 +0200

    changes

--- src/lib/server/db/server.ts
+++ src/lib/server/db/server.ts
@@ -1,10 +1,11 @@
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
+import { DATABASE_URL, TURSO_AUTH_TOKEN } from '$env/static/private';
 import * as schema from './schema.js';
 
 const client = createClient({
-	url: process.env.DATABASE_URL!,
-	authToken: process.env.TURSO_AUTH_TOKEN,
+	url: DATABASE_URL,
+	authToken: TURSO_AUTH_TOKEN,
 });
 
 export const db = drizzle(client, { schema });
